9d989b5c891b66e294d38fa74582b4036225fdad
David Wickes <dave.wickes@gmail.com>
2014-09-26 22:13:03 +0100

    Challenge... completed (and even on the same day). Now to refactor ideally.

--- lib/questions.rb
+++ lib/questions.rb
@@ -113,5 +113,84 @@ def round_down_number(number)
 end
 
 def format_date_nicely(date)
+  date.strftime('%d/%m/%Y')
+end
 
+def get_domain_name_from_email_address(email_address)
+  email_address.gsub(/.+@([^.]+).+/, '\1')
+end
+
+def titleize_a_string(string)
+  title = string.split(' ').map do |word|
+    ignored_words = ['and','the','on','a','in','of']
+    ignored_words.include?(word) ? word : word.capitalize
+  end
+  title.first.capitalize!
+  title.join(' ')
+end
+
+def check_a_string_for_special_characters(string)
+  special_characters = ["!","@"]
+  special_characters.each do |character|
+    return true if string.chars.include?(character)
+  end
+  return false
+end
+
+def get_upper_limit_of(range)
+  range.end
+end
+
+def is_a_3_dot_range?(range)
+  range.exclude_end?
+end
+
+def square_root_of(number)
+  Math.sqrt(number)
+end
+
+def word_count_a_file(file_name)
+  word_count = 0
+  File.open("./#{file_name}", "r") do |file|
+    file.each_line do |line|
+      word_count += line.split(' ').length
+    end
+  end
+  word_count
+end
+
+def is_a_2014_bank_holiday?(date)
+  bank_holidays = [
+    Time.new(2014,1,1),
+    Time.new(2014,4,18),
+    Time.new(2014,4,21),
+    Time.new(2014,5,5),
+    Time.new(2014,5,26),
+    Time.new(2014,8,25),
+    Time.new(2014,12,25),
+    Time.new(2014,12,26)
+  ]
+  bank_holidays.include?(date)
+end
+
+def your_birthday_is_on_a_friday_in_the_year(birthday)
+  #Tiem? Wish it were Daet!
+  loop do
+    return birthday.year if  birthday.friday?
+    birthday = Time.new(*(birthday.to_a[5]+=1))
+  end
+end
+
+def count_words_of_each_length_in_a_file(file_name)
+  count_of_words_and_length = Hash.new(0)
+  punctuation = ["'", ",", "."]
+  File.open("./#{file_name}", "r") do |file|
+    file.each_line do |line|
+      punctuation.each {|mark| line.delete!(mark)}
+      line.split(' ').each do |word|
+        count_of_words_and_length[word.length] += 1
+      end
+    end
+  end
+  count_of_words_and_length
 end
